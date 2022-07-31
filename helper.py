d5b764574a458b4aeeaa472715885423ec1ba623
lekhit borole <blekhit@gmail.com>
2022-07-31 22:45:37 +0530

    removed print statements

--- helper.py
+++ helper.py
@@ -5,7 +5,5 @@ X = sparse.load_npz(basepath+"data/yourmatrix.npz")
 with open(basepath+'data/neigh.pkl','rb') as f:
     neigh=pickle.load(f)
 def send_index(index):
-	print(index)
 	remm=neigh.kneighbors(X[index:index+1],n_neighbors=100,return_distance=False)
-	print(list(remm[0]))
 	return remm[0].tolist()
